eeb0ee398df4544c2a6d052f7060c585faa0a92f
kioie <kioi@outlook.com>
2020-04-23 21:42:13 +0300

    Added App.yaml file for instance configs

--- src/main/java/com/gcp/springboot/inventorymanagement/model/Inventory.java
+++ src/main/java/com/gcp/springboot/inventorymanagement/model/Inventory.java
@@ -23,10 +23,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 
 @Entity
+@Table(name = "inventory")
 public class Inventory {
 
     @Id
